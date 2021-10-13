8e3be602eee117b9de6567306692c192d753bf80
siktro <siktro@gmail.com>
2021-10-14 01:41:19 +0300

    some refactoring and new features

---
+++ README.md
@@ -0,0 +1,26 @@
+# Simple Web API written in Go
+
+For practice and learning sake.
+
+> `.env` is a dummy file with no real credentials.
+
+## Endpoints
+
+- `GET /books` - get all books;
+- `GET /books/{id}` - get a book by id;
+- `POST /books` - add a book;
+- `PUT /books` - update a book;
+- `DELETE /books/{id}` - delete a book by id.
+
+# Run
+
+1. `docker compose up -d` - run DB with GUI.
+2. `go run main.go`
+
+# TOOD
+
+- [ ] Start containers from server itself?
+- [ ] Add some middleware.
+- [ ] Add auth.
+- [ ] ORM?
+- [ ] Add some 'seed' commands to populate DB.
--- main.go
+++ main.go
@@ -2,24 +2,20 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
 
+	"github.com/siktro/books-api/controllers"
 	"github.com/siktro/books-api/database"
-	"github.com/siktro/books-api/models"
-
-	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
 
 func main() {
+	logger := log.New(os.Stdout, "[>] ", log.LstdFlags)
 
 	// Setup env. variables.
 	err := setEnvFromFile("./.env")
@@ -37,133 +33,8 @@ func main() {
 	logFatal("opening database", err)
 	defer closer()
 
-	router := mux.NewRouter()
-
-	router.HandleFunc("/books", getBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/books", addBook).Methods("POST")
-	router.HandleFunc("/books", updBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", delBook).Methods("DELETE")
-
-	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	var books []models.Book
-	book := models.Book{}
-
-	rows, err := db.Query("SELECT * FROM books")
-	if err != nil {
-		logFatal("reporting to client", err)
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		if err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			logFatal("reporting to client", err)
-		}
-		books = append(books, book)
-	}
-
-	if err = json.NewEncoder(w).Encode(books); err != nil {
-		logFatal("encoding json", err)
-	}
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	book := models.Book{}
-
-	row := db.QueryRow("SELECT * FROM books WHERE id = $1", params["id"])
-	err := row.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			w.Write([]byte("no rows"))
-		}
-		logFatal("scanning row", err)
-	}
-
-	if err = json.NewEncoder(w).Encode(book); err != nil {
-		logFatal("encoding json", err)
-	}
-}
-
-func addBook(w http.ResponseWriter, r *http.Request) {
-	var book models.Book
-
-	err := json.NewDecoder(r.Body).Decode(&book)
-	if err != nil {
-		logFatal("decoding json", err)
-	}
-
-	row := db.QueryRow(`
-		INSERT INTO books(id, title, author, year)
-		VALUES(default, $1, $2, $3)
-		RETURNING id`,
-		book.Title, book.Author, book.Year,
-	)
-	err = row.Scan(&book.ID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			w.Write([]byte("no rows"))
-		} else {
-			logFatal("querying db", err)
-		}
-	}
-
-	json.NewEncoder(w).Encode(book.ID)
-}
-
-func updBook(w http.ResponseWriter, r *http.Request) {
-	var book models.Book
-
-	err := json.NewDecoder(r.Body).Decode(&book)
-	if err != nil {
-		logFatal("decoding json", err)
-	}
-
-	res, err := db.Exec(`
-		UPDATE books SET
-			title = $1,
-			author = $2,
-			year = $3
-		WHERE
-			id = $4
-		RETURNING id
-	`, &book.Title, &book.Author, &book.Year, &book.ID)
-
-	if err != nil {
-		logFatal("querying db", err)
-	}
-
-	rf, err := res.RowsAffected()
-	if err != nil {
-		logFatal("querying db", err)
-	}
-
-	json.NewEncoder(w).Encode(rf)
-}
-
-func delBook(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-
-	res, err := db.Exec(`
-		DELETE FROM books
-		WHERE id = $1
-	`, params["id"])
-
-	if err != nil {
-		logFatal("querying db", err)
-	}
-
-	rf, err := res.RowsAffected()
-	if err != nil {
-		logFatal("querying db", err)
-	}
-
-	json.NewEncoder(w).Encode(rf)
+	api := controllers.MakeAPI(db, logger)
+	log.Fatal(http.ListenAndServe(":8000", api))
 }
 
 // Utils.
